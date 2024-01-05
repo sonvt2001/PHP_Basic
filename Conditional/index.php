4712aad2a78fdd80fbfd5f5e9fa381a3d15558a6
sonw <sonvt.la@gmail.com>
2024-01-05 09:58:43 +0700

    final conditional basic in php

--- Conditional/index.php
+++ Conditional/index.php
@@ -1,4 +1,43 @@
 <?php
 
+    $a = 10;
+    $b = 5;
 
+    if( $a > $b )
+    {
+        echo $a . " > " . $b . "</br>";
+    }
+    else 
+    {
+        echo $a . " < " . $b . "</br>";
+    }
+
+    $name = "truc";
+    if( $name == "son")
+    {
+        echo "
+            <li>Name : SON </li>
+            <li>Age : 23 </li>
+        ";
+    }
+    elseif( $name == "truc" )
+    {
+        echo "
+            <table>
+                    <caption style = 'color: green;'>Student Information</caption>
+                <tr>
+                    <th>Name</th>
+                    <th>Age</th>
+                </tr>
+                <tr>
+                    <td>TRUC</td>
+                    <td>19</td>
+                </tr>
+            </table>
+        ";
+    }
+    else
+    {
+        echo "<p style = 'color: red'>There are no students with " . "'$name'" . " in the class<p>";
+    }
 ?>
